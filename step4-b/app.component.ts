19ae9e7c8a8f974189c696846e54a943510f8f74
manuelfernando <manuelcamaron@hotmail.com>
2018-02-24 10:39:10 -0500

    add step4-b

---
+++ step4-b/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatEngine } from './chatEngine';
+declare var require: any;
+const random = require('chat-engine-random-username');
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+  private ce: any;
+
+  constructor(private chatEngine: ChatEngine) {
+    this.ce = chatEngine;
+  }
+
+  ngOnInit() {
+    this.ce.instance.connect(new Date().getTime(), {}, 'auth-key');
+    this.ce.instance.on('$.ready', (data) => {
+      this.ce.me = data.me;
+      this.ce.me.plugin(random());
+
+      this.ce.chat = this.ce.instance.global;
+    });
+  }
+}
