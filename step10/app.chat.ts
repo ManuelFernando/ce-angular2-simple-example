a625abb3ec852b6c3be4e1d0852bdcba2dbb2495
manuelfernando <manuelcamaron@hotmail.com>
2018-02-27 16:51:30 -0500

    add step10

---
+++ step10/app.chat.ts
@@ -0,0 +1,52 @@
+import {Component, Input, OnInit } from '@angular/core';
+import {ChatEngine} from './chatEngine';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './templates/app.chat.html'
+})
+export class AppChatComponent implements OnInit {
+  private ce: any;
+  @Input() chat: any;
+  @Input() index: number;
+  users: any[] = [];
+  messages: any[] = [];
+  message: string;
+  mysearch: string = '';
+  users: any[] = [];
+
+  constructor(private chatEngine: ChatEngine) {
+    this.ce = chatEngine;
+  }
+
+  ngOnInit() {
+    ...
+  }
+
+  getUsers(obj) {
+    ...
+  }
+
+  invite(user) {
+    this.chat.invite(user);
+    this.users = [];
+
+    return false;
+  }
+
+  leave() {
+    ...
+  }
+
+  send() {
+    ...
+  }
+
+  search() {
+    if (this.mysearch.length >= 2) {
+      this.users = this.ce.chat.onlineUserSearch.search(this.mysearch);
+    } else {
+      this.users = [];
+    }
+  }
+}
