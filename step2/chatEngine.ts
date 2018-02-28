b9707a5e55c38e9edf1e5af0b74bf3daaa4c504d
manuelfernando <manuelcamaron@hotmail.com>
2018-02-27 19:15:45 -0500

    remove keys

--- step2/chatEngine.ts
+++ step2/chatEngine.ts
@@ -9,8 +9,8 @@ export class ChatEngine {
   constructor() {
     this.instance = ChatEngineCore.create(
       {
-        publishKey: 'pub-c-d8599c43-cecf-42ba-a72f-aa3b24653c2b',
-        subscribeKey: 'sub-c-6c6c021c-c4e2-11e7-9628-f616d8b03518'
+        publishKey: 'YOUR PUBLISH KEY HERE',
+        subscribeKey: 'YOUR SUBSCRIBE KEY HERE'
       },
       {
         debug: true,
